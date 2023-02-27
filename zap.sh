efb1f7039c18df0a5d3578c6fb921dfd9d4d32ad
Fernando Andres Mora <59289595+fer9003@users.noreply.github.com>
2023-02-27 09:46:33 -0500

    changes script zap.sh

--- zap.sh
+++ zap.sh
@@ -9,7 +9,7 @@ docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t 
 exit_code=$?
 #exit_code = 0 no vulnerabilities
 sudo mkdir -p owasp-zap-report
-sudo mv zap_report.html owasp-zap-report
+sudo cp zap_report.html owasp-zap-report
 
 echo "Exit Code: $exit_code"
 
